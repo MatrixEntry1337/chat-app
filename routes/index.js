f9f27bd03cd240aac39c3be95b835ea6dbe28c77
redbattousai <kchangfatt@gmail.com>
2016-11-24 02:18:28 +0000

    Edited background color and App name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Revature Group Chat Room' });
+  res.render('index', { title: 'Erase Me' });
 });
 
 module.exports = router;
